b395d8fee58875d8ec44e95b2e3b2be8746bc7cb
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-22 07:05:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+let array = ["Ada","Brendan","Ali"];
+
+function writeCards(array, event){
+  let arrNew = [];
+  
+  for(let i =0; i < array.length; i++ ){
+    console.log("Thanks" + array[i] + );
+    
+  }
+  
+  return array
+}
